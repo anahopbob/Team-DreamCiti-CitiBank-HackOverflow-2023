387227cdfa024a4d4ca566e16c6eb20b1ea002f6
Xun Yi <xunyi.work@gmail.com>
2023-09-04 23:12:19 +0800

    Moved chroma client out, added enroll endpoint

--- backend/app.py
+++ backend/app.py
@@ -1,56 +1,86 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Request
+from pydantic import BaseModel
+
+from chroma_service import DocumentParser
 
 import chromadb
+
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import ElasticVectorSearch, Pinecone, Weaviate, FAISS
 from langchain.chains.question_answering import load_qa_chain
 
+# Initialize FastAPI
 app = FastAPI()
+
+# Initialize chroma db client 
+client = chromadb.PersistentClient(
+                                path="db/chroma.db"
+                                )   
+
+# Document Item
+class Document(BaseModel):
+    id:str #From s3 bucket
+    text:str
+    department:str
+
 
 @app.get("/")
 async def root():
     return { "message": "Hello world" }
 
+@app.post("/enroll")
+def enroll(document: Document):
+    """
+    Given a particular department and text, get embeddings for the text 
+    and enroll inside the DB.
+    """
+    document_dict = document.dict()
+    id = document_dict.get("id","")
+    text = document_dict.get("text","")
+    department = document_dict.get("department","")
 
-@app.get("/search/{query}/{file}")
-def search(query: str, file: str):
-    reader = PdfReader(file)
-    raw_text = ""
-    for i, page in enumerate(reader.pages):
-        text = page.extract_text()
-        if text:
-            raw_text += text
+    texts, chunk_ids = DocumentParser.split_texts(text)
+    print(f"Length of texts is {len(text)}")
+    return texts
 
-    text_splitter = CharacterTextSplitter(
-        separator = "\n",
-        chunk_size = 1500, # need to research on the right value to use for chunk_size and chunk_overlap
-        chunk_overlap = 200,
-        length_function = len,
-    )
-    
-    texts = text_splitter.split_text(raw_text)
-    
-    chunk_ids = []
-    index = 1
-    for text in texts:
-        chunk_ids.append(f"id{index}")
-        index+=1
-    
 
-    chroma_client = chromadb.Client()
+# @app.get("/search/{query}/{file}")
+# def search(query: str, file: str):
+#     reader = PdfReader(file)
+#     raw_text = ""
+#     for i, page in enumerate(reader.pages):
+#         text = page.extract_text()
+#         if text:
+#             raw_text += text
+
+#     text_splitter = CharacterTextSplitter(
+#         separator = "\n",
+#         chunk_size = 1500, # need to research on the right value to use for chunk_size and chunk_overlap
+#         chunk_overlap = 200,
+#         length_function = len,
+#     )
+    
+#     texts = text_splitter.split_text(raw_text)
+    
+#     chunk_ids = []
+#     index = 1
+#     for text in texts:
+#         chunk_ids.append(f"id{index}")
+#         index+=1
+    
     
         
-    collection = chroma_client.get_or_create_collection(name="dreamciti")
-    collection.add(documents=texts, ids=chunk_ids)
+#     collection = chroma_client.get_or_create_collection(name="dreamciti")
+#     collection.add(documents=texts, ids=chunk_ids)
 
-    # Use 5 Chunks of text to do the similarity search
-    results = collection.query(
-        query_texts=[query],
-        n_results=5,
-    )
+#     # Use 5 Chunks of text to do the similarity search
+#     results = collection.query(
+#         query_texts=[query],
+#         n_results=5,
+#     )
 
-    return results
+#     return results
 
 
 
