c197ec5ac7c91a31eb0b86cac5ca9878e4933f4e
Xun Yi <xunyi.work@gmail.com>
2023-09-05 13:56:44 +0800

    Added in first implementation of check_broken_tags

--- backend/app/services/chroma_service.py
+++ backend/app/services/chroma_service.py
@@ -1,5 +1,6 @@
 import re
 import uuid
+from typing import List
 
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
@@ -82,12 +83,63 @@ class DocumentParser():
                 output.append((type, object_id))
         return output
     
-    # @staticmethod
-    # def check_broken_tags(text:str):
+    @staticmethod
+    def check_broken_tags(text:str, tag_max_length:int=50) -> List[bool]:
+        """
+        Given a text, scans to see if there are broken text.
+        Looks out for <%? and %>
+        If we are using UUID, the expected length of the text will be 36 character. 
 
+        Returns:
+            [bool,bool]: True indicates the tag is broken, False indicates the tag is unbroken,
+                        First element refers to start, second element refers to end.
+        """
+        
+        output = [False, False]
+
+        text_start = text[:tag_max_length]
+        text_end = text[-tag_max_length:]
+
+        first_index = text_start.find(">")
+        if first_index != -1:
+            # Means there might be broken tag at the start
+            if first_index - 1 >= 0 and text_start[first_index-1] == "%": # %>
+                if text_start.find("<?%") != -1:
+                    # This means the tag is complete
+                    output[0] = False
+                else:
+                    output[0] = True
+        
+        # Find all occurences of "<" in end
+        last_index = text_end.rfind("<")
+        if last_index != -1:
+            # Means there might be broken tag at the end
+            # Find index of "<"
+            # Check if the next character is "%?"
+            if last_index + 1 < len(text_end) and text_end[last_index+1] == "?": # <? 
+                if last_index + 2 < len(text_end) and text_end[last_index+2] == "%": # <?%
+                    # Check if from the last_index to the end, is there a "%>"
+                    if "%>" in text_end[last_index:]:
+                        # This means the tag is unbroken
+                        output[1] = False
+                    else:
+                        output[1] = True
+                
+        return output
 
 if __name__ == "__main__":
-    text = f"of life's cruelties. <?% type=image,object_id={str(uuid.uuid4())[:-6]} %> <?% type=image,object_id={str(uuid.uuid4())[:-6]} %> Were they necessary for growth, like the way heat transforms sugar into the sweetest candies? Or were they simply random, like a child's choice of which jelly bean to eat next? <?% type=text,object_id={str(uuid.uuid4())[:-6]} %> In that moment, as it melted away in the warmth of a child's mouth, Jello found a bitte"
-    texts, ids = DocumentParser.parse_raw_texts(text)
-    print(texts)
-    print(ids)
+    # text = f"of life's cruelties. <?% type=image,object_id={str(uuid.uuid4())[:-6]} %> <?% type=image,object_id={str(uuid.uuid4())[:-6]} %> Were they necessary for growth, like the way heat transforms sugar into the sweetest candies? Or were they simply random, like a child's choice of which jelly bean to eat next? <?% type=text,object_id={str(uuid.uuid4())[:-6]} %> In that moment, as it melted away in the warmth of a child's mouth, Jello found a bitte"
+    # texts, ids = DocumentParser.parse_raw_texts(text)
+    # print(texts)
+    # print(ids)
+
+    text1 = "This is a sample text without a broken tag: <?% type=broken,object_id=123 %>. Lorem ipsum dolor sit amet."
+    text2 = "<<<<?% type=extra<,object_id=456 %>>. Nulla < >euismod massa vel lectus."
+    text3 = "This is a sample text with a broken tag: <?% type=broken,object_id=123"
+    text4 = "ect_id=123 %>This is a sample text with a broken tag:<?% type=broken,object_id=123"
+
+    assert DocumentParser.check_broken_tags(text1) == [False, False], f"Expected [False, False], got {DocumentParser.check_broken_tags(text1)}!"
+    assert DocumentParser.check_broken_tags(text2) == [False, False], f"Expected [False, False], got {DocumentParser.check_broken_tags(text2)}!"
+    assert DocumentParser.check_broken_tags(text3) == [False, True], f"Expected [False, True], got {DocumentParser.check_broken_tags(text3)}!"
+    assert DocumentParser.check_broken_tags(text4) == [True, True], f"Expected [True, True], got {DocumentParser.check_broken_tags(text4)}!"
+    
