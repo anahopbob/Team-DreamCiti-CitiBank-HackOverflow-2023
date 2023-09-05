1342ea2fc543b4c7fe6de56b8108bfc6ffdfbb59
KeithLoh <keithloh99@gmail.com>
2023-09-06 01:09:20 +0800

    feat: fix structure of FE add upload PDF component

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,12 +1,12 @@
-import React from "react";
-
 import Layout from "../components/Layout";
 
-import Home from "../components/Home/HomePage";
+import Home from "../pages/HomePage";
+import Upload from "../pages/UploadPage";
 import { createBrowserRouter } from "react-router-dom";
 
 export enum Routes {
   Home = "/",
+  Upload = "/upload",
 }
 
 const AppRoutes = createBrowserRouter([
@@ -14,6 +14,7 @@ const AppRoutes = createBrowserRouter([
     element: <Layout />,
     children: [{ path: Routes.Home, element: <Home /> }],
   },
+  { path: Routes.Upload, element: <Upload /> },
 ]);
 
 export default AppRoutes;
