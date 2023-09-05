8cbb49ecc84fa1a8976f4caf9906c72ca16e6179
Xun Yi <xunyi.work@gmail.com>
2023-09-05 11:27:15 +0800

    Created extract tags

--- backend/chroma_service.py
+++ backend/chroma_service.py
@@ -1,8 +1,13 @@
+import re
+import uuid
+
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 from pathlib import Path
 
 class DocumentParser():
+    # TAGS_REGX  = r'<\?%\s*.*?%\s*>'
+    TAGS_REGX  = r'<\?%\s*(.*?)%\s*>'
 
     @staticmethod
     def parse_pdf(file:Path):
@@ -34,4 +39,25 @@ class DocumentParser():
             index+=1
         
         return texts, chunk_ids
+    
+    @staticmethod
+    def extract_tags(text:str):
+        """
+        Given an excerpt, extract out the selected tags
+        """
+        matches = re.findall(DocumentParser.TAGS_REGX, text)
+        output = []
+        if len(matches) > 0:
+            for match in matches:
+                type, object_id = match.split(",")
+                type = type.split("=")[1]
+                object_id = object_id.split("=")[1]
+                assert len(type) != 0 and len(object_id) != 0, f"Invalid tag format, {type, object_id}!" 
+                output.append((type, object_id))
+        return output
 
+if __name__ == "__main__":
+    text = f"of life's cruelties. <?% type=image,object_id={str(uuid.uuid4())} %> Were they necessary for growth, like the way heat transforms sugar into the sweetest candies? Or were they simply random, like a child's choice of which jelly bean to eat next? <?% type=text,object_id={str(uuid.uuid4())} %> In that moment, as it melted away in the warmth of a child's mouth, Jello found a bitte"
+    print(text)
+    matches = DocumentParser.extract_tags(text)
+    print(matches)
