776d787d4bc10e5348698c57db71ae6b2a417450
Tyler Lian <65641550+anahopbob@users.noreply.github.com>
2023-09-05 23:17:47 +0800

    Search function

--- frontend/src/pages/Search.tsx
+++ frontend/src/pages/Search.tsx
@@ -1,28 +1,51 @@
 import { useState } from 'react'
 import '../assets/styles/App.css'
-
+import Searchbar from '../components/Searchbar';
 function App() {
-  const [data, setData] = useState(null);
+    const departments = ['Finance', 'Sales', 'Marketing', 'HR', 'IT', 'Operations'];
+    const [dept, setDept] = useState("");
+    const [data, setData] = useState(null);
+    const [searchItem, setSearchItem] = useState('');
+    const [empty, setEmpty] = useState(false);
+    // Callback function to handle the search item
+    const handleSearch = (item: string, department: string) => {
+        if (item === '') {
+            setData(null);
+            setEmpty(true);
+            return;
+        }
+        setEmpty(false);
+        setSearchItem(item);
+        setDept(department);
 
-  const search = () => {
-    // Define the API endpoint URL
-    const apiUrl = 'http://127.0.0.1:8000/search/?department=finance&query=123131'; // Replace with your API endpoint
-    // Make the API call
-    fetch(apiUrl)
-      .then((response) => response.json())
-      .then((responseData) => setData(responseData))
-      .catch((error) => console.error('Error fetching data:', error));
-  };
-  return (
-    <>
-      <div>
-        TESTING APIS
-      </div>
-      <div>
-        <button onClick={search}>Data is:{JSON.stringify(data, null, 2)}</button>
-      </div>
-    </>
-  )
+        search(item, department);
+    };
+
+    const search = (query: string, department: string) => {
+        // Define the API endpoint URL
+        const apiUrl = 'http://127.0.0.1:8000/search/?department=' + department + '&query=' + query; // Replace with your API endpoint
+        // Make the API call
+        fetch(apiUrl)
+            .then((response) => response.json())
+            .then((responseData) => setData(responseData))
+            .catch((error) => console.error('Error fetching data:', error));
+    };
+    return (
+        <>
+            <div className="">
+                <Searchbar onSearch={handleSearch} departments={departments} />
+                <p>Search Item: {searchItem}</p>
+            </div>
+            {empty ? (
+                <p>Please enter something in the input field.</p>
+            ) : (
+                <span></span>
+            )}
+            <div>
+                Data is:{JSON.stringify(data, null, 2)}
+            </div>
+        </>
+    )
 }
 
 export default App
