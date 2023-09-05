7200a9ab476aedc4fd7f3e7f6a2b634c3ab5c212
Xun Yi <xunyi.work@gmail.com>
2023-09-05 14:47:06 +0800

    Added in fix_broken_tags

--- backend/app/services/chroma_service.py
+++ backend/app/services/chroma_service.py
@@ -102,9 +102,12 @@ class DocumentParser():
         text_end = text[-tag_max_length:]
 
         first_index = text_start.find(">")
-        if first_index != -1:
+        #Trouble shoot or   "> here, another tag here"
+        if "<?%" in text_start and "%>" in text_start:
+            output[0] = False
+        elif first_index != -1: 
             # Means there might be broken tag at the start
-            if first_index -1 > 0 : #This means the sentence starts with ">", cannot identify if it is broken. Will just say its true
+            if first_index -1 < 0 : #This means the sentence starts with ">", cannot identify if it is broken. Will just say its true
                 output[0] = True
             elif text_start[first_index-1] == "%": # %>
                 if text_start.find("<?%") != -1:
@@ -144,18 +147,47 @@ class DocumentParser():
         tag_max_length:int=TAGS_MAX_LEN
         )-> str:
         # Start is broken
+        index = full_text.find(excerpt) # Find the index of this in full step
         if indicator[0]: 
-            first_index = excerpt.find(">")
+            first_index = excerpt.find(">") # Assumption here is that everything on the left of this is the broken tag
+            # Add in the tags max length in front
+            if index - tag_max_length > 0: # 66 - 50 = 16 
+                excerpt = full_text[index - tag_max_length: index] + excerpt[first_index:]
+            else:
+                excerpt = full_text[:index] + excerpt[first_index:]
+        # End is broken
+        if indicator[1]:
+            last_index = excerpt.rfind("<")
+            index += len(excerpt) # Get the end index in full text
+            if index + tag_max_length < len(full_text):
+                excerpt = excerpt + full_text[last_index+1: index + tag_max_length]
+            else:
+                excerpt = excerpt + full_text[last_index+1:]
+        # Sometimes when you extend the excerpt, it might have another broken part. 
+        # In order to prevent this, we will trim 
 
+        return excerpt
+    
+    @staticmethod
+    def trim_broken_text(text:str):
+        opening_tag = text.find("<?%")
+        ending_tag = text.find("%>")
+        if ending_tag < opening_tag: # Means "%> <?%...%>" 
+            text = text[ending_tag+2:]
 
-
+        opening_tag = text.rfind("<?%")
+        ending_tag = text.rfind("%>")
+        if opening_tag > ending_tag:
+            text = text[:opening_tag]
+        return text
 
 if __name__ == "__main__":
     # text = f"of life's cruelties. <?% type=image,object_id={str(uuid.uuid4())[:-6]} %> <?% type=image,object_id={str(uuid.uuid4())[:-6]} %> Were they necessary for growth, like the way heat transforms sugar into the sweetest candies? Or were they simply random, like a child's choice of which jelly bean to eat next? <?% type=text,object_id={str(uuid.uuid4())[:-6]} %> In that moment, as it melted away in the warmth of a child's mouth, Jello found a bitte"
     # texts, ids = DocumentParser.parse_raw_texts(text)
     # print(texts)
     # print(ids)
-
+    
+    # Asserts broken tag is working
     text1 = "This is a sample text without a broken tag: <?% type=broken,object_id=123 %>. Lorem ipsum dolor sit amet."
     text2 = "<<<<?% type=extra<,object_id=456 %>>. Nulla < >euismod massa vel lectus."
     text3 = "This is a sample text with a broken tag: <?% type=broken,object_id=123"
@@ -166,3 +198,18 @@ if __name__ == "__main__":
     assert DocumentParser.check_broken_tags(text3) == [False, True], f"Expected [False, True], got {DocumentParser.check_broken_tags(text3)}!"
     assert DocumentParser.check_broken_tags(text4) == [True, True], f"Expected [True, True], got {DocumentParser.check_broken_tags(text4)}!"
     
+    # Test fix_broken_tag
+    fulltext = "This is the full text with the tags <?% type=extra,object_id=456 %> here, another tag here <?% type=extra,object_id=456 %>, one more tag here!"
+    excerpt1 = "pe=extra<,object_id=456 %> here, another tag here <?% type=extra,object_id=45"
+    excerpt2 = "This is the full text with the tags <"
+    excerpt3 = "> here, another tag here"
+
+    fixed_excerpt1 = DocumentParser.trim_broken_text(DocumentParser.fix_broken_tags(excerpt1, fulltext, DocumentParser.check_broken_tags(excerpt1)))
+    fixed_excerpt2 = DocumentParser.trim_broken_text(DocumentParser.fix_broken_tags(excerpt2, fulltext, DocumentParser.check_broken_tags(excerpt2)))
+    fixed_excerpt3 = DocumentParser.trim_broken_text(DocumentParser.fix_broken_tags(excerpt3, fulltext, DocumentParser.check_broken_tags(excerpt3)))
+
+    print(fixed_excerpt1)
+    print(fixed_excerpt2)
+    print(fixed_excerpt3)
+   
+
