d5b4ce71f9be5455d0e87aea016036576e4382f6
Xun Yi <95530032+peek00@users.noreply.github.com>
2023-09-06 20:24:20 +0800

    Linked up mysql

--- backend/main.py
+++ backend/main.py
@@ -1,21 +1,34 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session  # Import Session
 from app.routes import chromaDB  # Import your API route modules
 from fastapi.middleware.cors import CORSMiddleware
+from app.routes.relationalDatabase import initialize_rdbs, insert_dummy_data, Object_Table, ObjectVotes # Import RDBMS
 
 # Initialize FastAPI
 app = FastAPI()
 
-# Document Item for POST Enroll
+# Initalize DB
+def get_db_session():
+    session = initialize_rdbs()
+    # insert_dummy_data(session)  # You may want to call this only once when initializing the app
+    try:
+        yield session
+    finally:
+        session.close()
 
-# Initialize chroma db client 
+@app.get("/get-object/{object_id}")
+def get_object_by_id(
+        object_id: str,
+        session: Session = Depends(get_db_session)
+      ):
+    # Use SQLAlchemy to query the database based on the object_id
+    object_data = session.query(Object_Table).filter(Object_Table.ObjectID == object_id).first()
 
-
-@app.get("/")
-async def root():
-    return { "message": "Hello world" }
-
-
-
+    if object_data is None:
+        # Handle the case where the object_id doesn't exist in the database
+        return {"message": "Object not found"}
+    return object_data
+    
 # CORS (Cross-Origin Resource Sharing) configuration
 app.add_middleware(
     CORSMiddleware,
